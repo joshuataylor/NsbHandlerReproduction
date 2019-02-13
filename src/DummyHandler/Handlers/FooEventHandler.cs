d6a235f1624d75e58868ae4c3c029e2cde5d67f2
Josh Taylor <joshuataylorx@gmail.com>
2019-02-13 14:33:48 +0800

    Fixes #1 - Remove MemoryStream

--- src/DummyHandler/Handlers/FooEventHandler.cs
+++ src/DummyHandler/Handlers/FooEventHandler.cs
@@ -1,5 +1,4 @@
 using System;
-using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using DummyMessages;
@@ -12,8 +11,6 @@ namespace DummyHandler.Handlers
         // Just send the BarEvent here
         public async Task Handle(FooEvent message, IMessageHandlerContext context)
         {
-            using (var ms = new MemoryStream())
-            {
                 Console.WriteLine($"Received Message with ID {message.Id}, sleeping 30ms seconds to simulate normal usage");
                 Thread.Sleep(30);
 
@@ -31,7 +28,6 @@ namespace DummyHandler.Handlers
                     await context.Publish(barEvent).ConfigureAwait(false);
                     Console.WriteLine($"Sent BarEvent with ID {barEvent.Id}");
                 }
-            }
         }
     }
 }
